92f026e0d6bd0fcdf783deacf9574205f20579d7
Prasit Kaew. Champathong <prasit.c@cjworx.com>
2022-10-14 18:15:15 +0700

    test line reply message

---
+++ pages/api/line/webhook.ts
@@ -0,0 +1,39 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { ClientConfig, Client, WebhookEvent, TextMessage, MessageAPIResponseBase } from '@line/bot-sdk';
+
+const clientConfig: ClientConfig = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN || '',
+  channelSecret: process.env.CHANNEL_SECRET,
+};
+
+const client = new Client(clientConfig);
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.body.events.length === 0) {
+    res.status(200).json({
+      status: 'success'
+    })
+  }
+
+  const replyToken = req.body.events[0].replyToken
+
+  const response: TextMessage = {
+    type: 'text',
+    text: 'test'
+  };
+
+  // Reply to the user.
+  await client.replyMessage(replyToken, response);
+
+  // client.broadcast({
+  //   type: "text",
+  //   text: 'test'
+  // })
+  // .then(data => console.log(data))
+  // .catch(e => console.log(e))
+
+  res.status(200).json({})
+}
