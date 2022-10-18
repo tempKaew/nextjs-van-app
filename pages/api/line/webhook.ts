b5b0a240b047b6b2c3b77a657568c0a067a56b04
Prasit Kaew. Champathong <prasit.c@cjworx.com>
2022-10-18 10:51:26 +0700

    add response

--- pages/api/line/webhook.ts
+++ pages/api/line/webhook.ts
@@ -8,20 +8,51 @@ const clientConfig: ClientConfig = {
 
 const client = new Client(clientConfig);
 
-const textEventHandler = async (event: object) => {
-  const message = event.message.text
+const textEventHandler = async (event: WebhookEvent): Promise<MessageAPIResponseBase | undefined> => {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    return;
+  }
+  const { replyToken } = event;
+  const { text } = event.message;
   const response: TextMessage = {
     type: 'text',
-    text: 'return : ' + message
+    text: 'return : ' + text
   };
-  const replyToken = event.replyToken
   await client.replyMessage(replyToken, response);
+}
+
+const followEventHandler = async (event: WebhookEvent): Promise<MessageAPIResponseBase | undefined> => {
+  if (event.type !== 'follow') {
+    return;
+  }
+  const userId = event.source.userId
+  const { replyToken } = event;
+  const response: TextMessage = {
+    type: 'text',
+    text: 'welcome'
+  };
+  await client.replyMessage(replyToken, response);
+}
+
+const unFollowEventHandler = async (event: WebhookEvent): Promise<MessageAPIResponseBase | undefined> => {
+  if (event.type !== 'unfollow') {
+    return;
+  }
+  const userId = event.source.userId
+  console.log('unfollow userID: ',userId)
 }
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  if (req.method === 'GET') {
+    return res.status(405).json({
+      status: 'error',
+      message: 'Method Not Allowed'
+    });
+  }
+
   const events: WebhookEvent[] = req.body.events;
 
   if (events===undefined) {
@@ -32,9 +63,26 @@ export default async function handler(
 
   const results = await Promise.all(
     events.map(async (event: WebhookEvent) => {
+      console.log('event.type: ',event.type)
       if (event.type === 'message' && event.message.type === 'text') {
         try {
           await textEventHandler(event);
+        } catch (err: unknown) {
+          if (err instanceof Error) {
+            console.error(err);
+          }
+        }
+      }else if (event.type === 'follow') {
+        try {
+          await followEventHandler(event);
+        } catch (err: unknown) {
+          if (err instanceof Error) {
+            console.error(err);
+          }
+        }
+      }else if (event.type === 'unfollow') {
+        try {
+          await unFollowEventHandler(event);
         } catch (err: unknown) {
           if (err instanceof Error) {
             console.error(err);
